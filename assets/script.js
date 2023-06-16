b0521ffee6ee6aeebb81c305429742d9fdd43de7
MiguelKoh <81209063+MiguelKoh@users.noreply.github.com>
2023-06-16 10:12:29 -0600

    version 16 de junio

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,10 @@
-function getHost() {
-  return window.location.hostname;
-}
 
 
 const imprimirCartas = async () => {
   
   try {
-    const hostname = getHost();
     const wrapper = document.querySelector(".swiper-wrapper");
-    const respuestaCartas = await fetch(`http://${hostname}/loteria/obtenerCarta.php`);
+    const respuestaCartas = await fetch("./obtenerCarta.php");
     const cartas = await respuestaCartas.json();
     let card = '';
   
